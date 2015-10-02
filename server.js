446cfb76277f6611765db2bc8ece14a0e9f04d96
NgaNguyenDuy <kyo1508@gmail.com>
2015-10-02 14:16:23 +0800

    update validation for connections mysql

--- server.js
+++ server.js
@@ -30,15 +30,42 @@ function handler (req, res) {
 // Loading config mysql
 var configMysql = require("./config.json");
 
-var connections = mysql.createConnection(configMysql);
+var connections;
 
-connections.connect((err) => {
-	if (err) {
-		console.log("Error: " + err.stack);
-	} else {
-		console.log("Connect to mysql successeful as id " + connections.threadId);
-	};
-});
+var handleDisconnect = () => {
+	connections = mysql.createConnection(configMysql); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  	connections.connect(function(err) {              // The server is either down
+    	if(err) {                                     // or restarting (takes a while sometimes).
+      		console.log('error when connecting to db:', err);
+      		setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    	} else {
+    		console.log("Connect to mysql successeful as id " + connections.threadId);
+    	}                                // to avoid a hot loop, and to allow our node script to
+  	});                                     // process asynchronous requests in the meantime.
+	                                          // If you're also serving http, display a 503 error.
+	connections.on('error', function(err) {
+	    console.log('db error', err);
+	    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+	      handleDisconnect();                         // lost due to either server restart, or a
+	    } else {                                      // connnection idle timeout (the wait_timeout
+	      throw err;                                  // server variable configures this)
+	    }
+	});	
+}
+
+handleDisconnect();
+
+// var connections = mysql.createConnection(configMysql);
+
+// connections.connect((err) => {
+// 	if (err) {
+// 		console.log("Error for custom: " + err.stack);
+// 	} else {
+// 		console.log("Connect to mysql successeful as id " + connections.threadId);
+// 	};
+// });
 
 
 io.on('connection', (socket) => {
